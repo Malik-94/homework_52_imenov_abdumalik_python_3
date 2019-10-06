26c0a3543e9bd7f9da77d9537902c7726edb3095
Malik-94 <imenov-a@mail.ru>
2019-10-07 00:13:13 +0600

    шаблоны статей сгруппированы в папке templates/status templates/task templates/type

--- source/webapp/views.py
+++ source/webapp/views.py
@@ -10,14 +10,14 @@ from webapp.forms import TypeForm
 class IndexView(View):
     def get(self, request, *args, **kwargs):
         task = Task.objects.all()
-        return render(request, 'index.html', context={
+        return render(request, 'task/index.html', context={
             'task': task
         })
 
 class TaskView(View):
     def get (self, request, pk):
         task = get_object_or_404(Task, pk=pk)
-        return render(request, 'task.html', context={
+        return render(request, 'task/task.html', context={
             'task': task
         })
 
@@ -25,7 +25,7 @@ class TaskCreateView(View):
     def get (self,request, *args, **kwargs):
         if request.method == 'GET':
             form = TaskForm()
-            return render(request, 'create.html', context={'form': form})
+            return render(request, 'task/create.html', context={'form': form})
 
     def post (self, request, *args, **kwargs):
         form = TaskForm(data=request.POST)
@@ -38,7 +38,7 @@ class TaskCreateView(View):
             )
             return redirect('task_view', pk=task.pk)
         else:
-            return render(request, 'create.html', context={'form': form})
+            return render(request, 'task/create.html', context={'form': form})
 
 
 class TaskUpdateView(View):
@@ -52,7 +52,7 @@ class TaskUpdateView(View):
                 'type': task.type,
                 'status': task.status
             })
-            return render(request, 'update.html', context={'form': form, 'task': task})
+            return render(request, 'task/update.html', context={'form': form, 'task': task})
 
     def post (self, request, pk, *args, **kwargs):
             form = TaskForm(data=request.POST)
@@ -65,14 +65,14 @@ class TaskUpdateView(View):
                 task.save()
                 return redirect('task_view', pk=task.pk)
             else:
-                return render(request, 'update.html', context={'form': form, 'task': task})
+                return render(request, 'task/update.html', context={'form': form, 'task': task})
 
 
 class TaskDeleteView(View):
     def get (self,request , pk, *args, **kwargs):
         task = get_object_or_404(Task, pk=pk)
         if request.method == 'GET':
-            return render(request, 'delete.html', context={'task': task})
+            return render(request, 'task/delete.html', context={'task': task})
 
     def post (self,request, *args, **kwargs):
         task = get_object_or_404(Task, pk=kwargs.get('pk'))
@@ -83,7 +83,7 @@ class TaskDeleteView(View):
 class StatusList(View):
     def get(self, request, *args, **kwargs):
         status = Status.objects.all()
-        return render(request, 'status_read.html', context={
+        return render(request, 'status/status_read.html', context={
             'status': status
         })
 
@@ -93,7 +93,7 @@ class StatusCreateView(View):
     def get (self,request, *args, **kwargs):
         if request.method == 'GET':
             form = StatusForm()
-            return render(request, 'status_create.html', context={'form': form})
+            return render(request, 'status/status_create.html', context={'form': form})
 
     def post (self, request, *args, **kwargs):
         form = StatusForm(data=request.POST)
@@ -104,7 +104,7 @@ class StatusCreateView(View):
             )
             return redirect('status_add')
         else:
-            return render(request, 'status_create.html', context={'form': form})
+            return render(request, 'status/status_create.html', context={'form': form})
 
 
 
@@ -115,12 +115,12 @@ class StatusUpdateView(View):
                 form = StatusForm(data={
                     'status': status
                 })
-            return render(request, 'status_update.html', context={'form': form, 'status': status})
+            return render(request, 'status/status_update.html', context={'form': form, 'status': status})
 
     def post (self, request, pk, *args, **kwargs):
             form = StatusForm(data=request.POST)
             status = get_object_or_404(Status, pk=pk)
-            return render(request, 'status_update.html', context={'form': form, 'status': status})
+            return render(request, 'status/status_update.html', context={'form': form, 'status': status})
 
 
 
@@ -128,7 +128,7 @@ class StatusDeleteView(View):
     def get (self,request , pk, *args, **kwargs):
         status = get_object_or_404(Status, pk=pk)
         if request.method == 'GET':
-            return render(request, 'status_delete.html', context={'status': status})
+            return render(request, 'status/status_delete.html', context={'status': status})
 
     def post (self,request,  *args, **kwargs):
         status = get_object_or_404(Status, pk=kwargs.get('pk'))
@@ -141,7 +141,7 @@ class StatusDeleteView(View):
 class TypeList(View):
     def get(self, request, *args, **kwargs):
         type = Type.objects.all()
-        return render(request, 'type_read.html', context={
+        return render(request, 'type/type_read.html', context={
             'type': type
         })
 
@@ -150,7 +150,7 @@ class TypeCreateView(View):
     def get(self, request, *args, **kwargs):
         if request.method == 'GET':
             form = TypeForm()
-            return render(request, 'type_create.html', context={'form': form})
+            return render(request, 'type/type_create.html', context={'form': form})
 
     def post(self, request, *args, **kwargs):
         form = TypeForm(data=request.POST)
@@ -161,7 +161,7 @@ class TypeCreateView(View):
             )
             return redirect('type_add')
         else:
-            return render(request, 'type_create.html', context={'form': form})
+            return render(request, 'type/type_create.html', context={'form': form})
 
 
 class TypeUpdateView(View):
@@ -171,19 +171,19 @@ class TypeUpdateView(View):
             form = TypeForm(data={
                 'type': type
             })
-        return render(request, 'type_update.html', context={'form': form, 'type': type})
+        return render(request, 'type/type_update.html', context={'form': form, 'type': type})
 
     def post(self, request, pk, *args, **kwargs):
         form = TypeForm(data=request.POST)
         type = get_object_or_404(Type, pk=pk)
-        return render(request, 'type_update.html', context={'form': form, 'type': type})
+        return render(request, 'type/type_update.html', context={'form': form, 'type': type})
 
 
 class TypeDeleteView(View):
     def get(self, request, pk, *args, **kwargs):
         type = get_object_or_404(Type, pk=pk)
         if request.method == 'GET':
-            return render(request, 'type_delete.html', context={'type': type})
+            return render(request, 'type/type_delete.html', context={'type': type})
 
     def post(self, request, *args, **kwargs):
         type = get_object_or_404(Type, pk=kwargs.get('pk'))
